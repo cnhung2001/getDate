5dc22a50f278ca453b8628b9bfb9fe647dff7835
cnhung2001 <hung1832001nb@gmail.com>
2023-09-12 23:12:31 +0700

    second commit

--- index.js
+++ index.js
@@ -4,30 +4,33 @@ import moment from "moment";
 // const month = 8; // Tháng 10 (0-11)
 
 function printAllDayInMonth(year, month) {
-  // Tạo một đối tượng Moment đại diện cho ngày đầu tiên trong tháng
-  const firstDayOfMonth = moment([year, month - 1]);
+  // Tạo một đối tượng Moment cho ngày đầu tiên của tháng
+  const firstDayOfMonth = moment({ year, month: month - 1, day: 1 });
 
-  // In ra tiêu đề cho lịch
-  console.log(firstDayOfMonth.format("MMMM YYYY"));
+  // Lấy số ngày trong tháng
+  const daysInMonth = firstDayOfMonth.daysInMonth();
+
+  // In ra tiêu đề của bảng
   console.log("Sun Mon Tue Wed Thu Fri Sat");
 
-  // Duyệt qua các ngày trong tháng
-  for (let i = 0; i < firstDayOfMonth.daysInMonth(); i++) {
-    const currentDay = firstDayOfMonth.clone().add(i, "days");
-
-    // In ra ngày và đánh dấu ngày hiện tại
-    if (i === 0) {
-      process.stdout.write(" ".repeat(currentDay.day() * 4)); // Đặt khoảng trống cho các ngày trước ngày đầu tiên trong tuần
+  // Bắt đầu từ ngày 1 của tháng
+  let currentDay = 1;
+  while (currentDay <= daysInMonth) {
+    let weekRow = "";
+    for (let i = 0; i < 7; i++) {
+      if (currentDay <= daysInMonth) {
+        const dayOfWeek = firstDayOfMonth.day();
+        if (i === dayOfWeek) {
+          weekRow += `${currentDay.toString().padStart(3)} `;
+          currentDay++;
+          firstDayOfMonth.add(1, "day");
+        } else {
+          weekRow += "    "; // Điền khoảng trắng cho các ô trống
+        }
+      }
     }
-
-    process.stdout.write(currentDay.date().toString().padStart(3, " "));
-
-    if (currentDay.day() === 6) {
-      console.log(); // Xuống dòng sau khi in xong ngày thứ 7 (Chủ Nhật)
-    } else {
-      process.stdout.write(" ");
-    }
+    console.log(weekRow);
   }
 }
 
-printAllDayInMonth(2023, 8);
+printAllDayInMonth(2023, 9);
